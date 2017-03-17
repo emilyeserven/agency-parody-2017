58d8370df1f81bd9412d89c9f5f01265a88e1a7e
emilyeserven <emily.e.serven@gmail.com>
2017-03-17 14:47:50 -0400

    typed text added

---
+++ js/typing.js
@@ -0,0 +1,9 @@
+$(function(){
+        $(".typing").typed({
+            strings: ["Innovators", "Creators"],
+            typeSpeed: 0,
+            loop: true,
+            typeSpeed: 25,
+            backSpeed: 50
+        });
+    });
